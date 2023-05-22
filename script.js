89685595e3fa62a35fd9187a91f24e768b3a7d1e
Floatingtales <58957425+floatingtales@users.noreply.github.com>
2023-05-22 18:47:21 +0700

    created coin flipper function:

---
+++ script.js
@@ -0,0 +1,13 @@
+var coinFlipper = function () {
+  var randomInteger = Math.floor(Math.random() * 2);
+  if (randomInteger == 1) {
+    return "heads";
+  } else {
+    return "tails";
+  }
+};
+
+var main = function (input) {
+  var myOutputValue = "hello world";
+  return myOutputValue;
+};
